9e8a5280d7d6827a6578526773234dbf13b20baf
Stefano Rampazzo <17473197+sramp@users.noreply.github.com>
2025-07-27 10:55:16 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftSQLite",
+    name: "AppSwiftSQLite",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SwiftSQLite",
-            targets: ["SwiftSQLite"]),
+            name: "AppSwiftSQLite",
+            targets: ["AppSwiftSQLite"]),
     ],
     dependencies: [
     ],
@@ -17,7 +17,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SwiftSQLite",
+            name: "AppSwiftSQLite",
             dependencies: []),
         .testTarget(
             name: "SwiftSQLiteTests",
